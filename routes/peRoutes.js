63ca358d348f103ec3278aef99b45961cc12dec9
Shayan <shayan.dev101@gmail.com>
2024-03-21 21:22:49 +0530

    form link change + box-shadow animation addition

--- routes/peRoutes.js
+++ routes/peRoutes.js
@@ -4,20 +4,29 @@ require('dotenv').config();
 const axios = require('axios');
 const uniqid = require('uniqid');
 const SHA256 = require('crypto-js/sha256');
+const CryptoJS = require('crypto-js');
 const http = require('http');
+const crypto = require('crypto');
 let app = express();
 app.use(express.urlencoded({ extended: true }));
 
+// const now = Date.now(); // 1675593338637
+
+// const timeString = now.toString();
+
+const randomBytes = CryptoJS.lib.WordArray.random(8);
+const hexString = randomBytes.toString(CryptoJS.enc.Hex);
+
 const config = {
-	hostUrl: process.env.PHONEPE_URL || 'https://api-preprod.phonepe.com/apis/pg-sandbox',
+	hostUrl: process.env.PHONEPE_URL, //|| 'https://api-preprod.phonepe.com/apis/pg-sandbox',
 	merchantId: process.env.MERCHANT_ID || 'PGTESTPAYUAT',
 	saltKey: process.env.SALT_KEY || '099eb0cd-02cf-4e2a-8aca-3e6c6aff0399',
 	saltIndex: process.env.SALT_INDEX || '1',
 	apiEndPoint: process.env.API_END_POINT || '/pg/v1/pay',
-	merchantTransactionId: uniqid(),
+	merchantTransactionId: hexString,
 	merchantUserId: uniqid(),
 };
-const agent = new http.Agent({ keepAlive: true });
+// const agent = new http.Agent({ keepAlive: true });
 
 router.get('/', (req, res) => {
 	res.render('pePaymentForm.ejs');
@@ -40,7 +49,7 @@ router.post('/pay', (req, res) => {
 	const payload = {
 		merchantId: config.merchantId,
 		merchantTransactionId: config.merchantTransactionId,
-		merchantUserId: config.merchantUserId,
+		merchantUserId: req.body.phone,
 		amount: 100 * req.body.amount, //take input from form //validate input
 		redirectUrl: `https://sawadeazam.org/pe/redirect-url/${config.merchantTransactionId}`,
 		redirectMode: 'REDIRECT',
@@ -53,46 +62,74 @@ router.post('/pay', (req, res) => {
 	console.log(payload);
 	const payloadBuffer = Buffer.from(JSON.stringify(payload));
 	const base64EncodedPayload = payloadBuffer.toString('base64');
-	console.log('base64EncodedPayload:', base64EncodedPayload);
 
 	const xVerify = SHA256(base64EncodedPayload + config.apiEndPoint + config.saltKey) + '###' + config.saltIndex;
 
 	console.log('xVerify:', xVerify);
-	const options = {
-		method: 'post',
-		url: `${config.hostUrl}/pg/v1/pay`,
+	// const options = {
+	// 	method: 'post',
+	// 	url: `${config.hostUrl}/pg/v1/pay`,
+	// 	headers: {
+	// 		// accept: 'application/json',
+	// 		'Content-Type': 'application/json',
+	// 		'X-VERIFY': xVerify,
+	// 	},
+	// 	data: {
+	// 		request: base64EncodedPayload,
+	// 	},
+	// 	// httpAgent: agent,
+	// 	// transformRequest: [
+	// 	// 	(data, headers) => {
+	// 	// 		delete headers.common.Expect;
+	// 	// 		return data;
+	// 	// 	},
+	// 	// ],
+	// };
+	// delete axios.defaults.headers['Expect'];
+	// axios
+	// 	.request(options)
+	// 	.then(function (response) {
+	// 		console.log('response-data:', response.data);
+	// 		if (response.data.success === true) {
+	// 			console.log(response.data.data.instrumentResponse.redirectInfo.url);
+	// 			const url = response.data.data.instrumentResponse.redirectInfo.url;
+	// 			return res.redirect(303, url);
+	// 		} else {
+	// 			console.error('Payment failed');
+	// 			return res.status(400).send('Payment failed');
+	// 		}
+	// 	})
+	// 	.catch(function (error) {
+	// 		console.error(error.message);
+
+	// 		return res.send(error.message);
+	// 	});
+
+	fetch(`${config.hostUrl}/pg/v1/pay`, {
+		method: 'POST',
 		headers: {
-			accept: 'application/json',
 			'Content-Type': 'application/json',
 			'X-VERIFY': xVerify,
 		},
-		data: {
+		body: JSON.stringify({
 			request: base64EncodedPayload,
-		},
-		// httpAgent: agent,
-		// transformRequest: [
-		// 	(data, headers) => {
-		// 		delete headers.common.Expect;
-		// 		return data;
-		// 	},
-		// ],
-	};
-	// delete axios.defaults.headers['Expect'];
-	axios
-		.request(options)
-		.then(function (response) {
-			console.log('response-data:', response.data);
-			if (response.data.success === true) {
-				console.log(response.data.data.instrumentResponse.redirectInfo.url);
-				const url = response.data.data.instrumentResponse.redirectInfo.url;
+		}),
+	})
+		.then((res) => res.json())
+		.then((data) => {
+			console.log('Payment response:', data);
+			if (data.success) {
+				const url = data.data.instrumentResponse.redirectInfo.url;
+				console.log(url);
 				return res.redirect(303, url);
-			} else return res.send(response.data);
+			} else {
+				console.error('Payment failed');
+				return res.status(400).send('Payment failed');
+			}
 		})
-		.catch(function (error) {
-			console.error(error.message);
-			console.log(error.response.status);
-			console.log(error.response.statusText);
-			return res.send(error.message);
+		.catch((err) => {
+			console.error('Error:', err.message);
+			return res.status(500).send('Error making payment');
 		});
 });
 
