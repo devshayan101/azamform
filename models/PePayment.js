7d0bc832f67f7a1d86e48b72300d25b78b27408f
Shayan <shayan.dev101@gmail.com>
2024-03-31 12:29:09 +0000

    payment time set time india time zone, suffa document submission link(google.forms)

--- models/PePayment.js
+++ models/PePayment.js
@@ -36,9 +36,19 @@ const pePaymentSchema = new mongoose.Schema(
 			type: String,
 			default: 'peForm',
 		},
-		createdAt: { type: Date, default: Date.now },
+		createdAt: {
+			type: Date,
+			default: Date.now, //  Set default value to current UTC time
+		},
 	},
 	// { timestamps: true, timezone: 'Asia/Kolkata' },
 );
+
+// Define a custom setter for the 'createdAt' field to set it to +05:30 time zone
+pePaymentSchema.path('createdAt').set(function (value) {
+	// Convert the value to Indian Standard Time (IST) (+05:30)
+	const istTime = new Date(value).toLocaleString('en-US', { timeZone: 'Asia/Kolkata' });
+	return new Date(istTime);
+});
 
 module.exports = mongoose.model('PePayment', pePaymentSchema);
